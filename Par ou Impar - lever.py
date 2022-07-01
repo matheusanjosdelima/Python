e478f5af0c65598be63a546a834704dad6ce0a3d
Matheus Anjos <35640034+matheusanjosdelima@users.noreply.github.com>
2022-06-30 22:46:24 -0300

    Update Par ou Impar - lever.py

--- Par ou Impar - lever.py
+++ Par ou Impar - lever.py
@@ -8,13 +8,13 @@ print('-=' * 29)
 print(f'{"VAMOS JOGAR PAR OU ÍMPAR":^56}')
 print('-=' * 29)
 while True:
-    valor = int(input('Digite um valor de 1 à 5: '))
-    while not 0 < valor <= 5:
+    valor = int(input('Digite um valor de 0 à 5: '))
+    while not 0 <= valor <= 5:
         print('')
         print('Valor Inválido. Tente novamente.')
         print('')
         valor = int(input('Digite um valor de 1 à 5: '))
-    computador = randint(1, 5)
+    computador = randint(0, 5)
     soma = valor + computador
     opcao = str(input('Par ou ímpar: ')).strip(). upper()
     while opcao != 'PAR' and opcao != 'ÍMPAR' and opcao != 'IMPAR':
@@ -26,17 +26,55 @@ while True:
     print(f'Você jogou {valor} e o computador {computador}. Total deu {soma}:', end=' ')
     if soma % 2 == 0:
         print('DEU PAR')
-        ganhador = 'PAR'
+        if opcao == 'PAR':
+            print('Você VENCEU!')
+            print('Vamos Jogar novamente...')
+            vitorias += 1
+        else:
+            print('Você PERDEU!')
+            print('-=' * 29)
+            print(f'GAME OVER!', end=' ')
+            if vitorias == 0:
+                print('Você não ganhou nenhuma vez.')
+                tentar_de_novo = str(input('Quer tentar de novo? [ S/N ] ')).strip().lower()
+                while tentar_de_novo != 'sim' and tentar_de_novo != 's' and tentar_de_novo != 'não' \
+                        and tentar_de_novo != 'nao' and tentar_de_novo != 'n':
+                    print('')
+                    print('Desculpe, não entendi. Tente novamente.')
+                    print('')
+                    tentar_de_novo = str(input('Quer tentar de novo? [ S/N ] ')).strip().lower()
+                if tentar_de_novo == 's' or tentar_de_novo == 'sim':
+                    vitorias = 0
+                else:
+                    break
+            else:
+                print(f'Você venceu {vitorias} {"vez" if vitorias == 1 else "vezes"}')
+                break
+
     else:
         print('DEU ÍMPAR')
-        ganhador = 0
+        if opcao == 'IMPAR' or opcao == 'ÍMPAR':
+            print('Você VENCEU!')
+            print('Vamos Jogar novamente...')
+            vitorias += 1
+        else:
+            print('Você PERDEU!')
+            print('-=' * 29)
+            print(f'GAME OVER!', end=' ')
+            if vitorias == 0:
+                print('Você não ganhou nenhuma vez.')
+                tentar_de_novo = str(input('Quer tentar de novo? [ S/N ] ')).strip().lower()
+                while tentar_de_novo != 'sim' and tentar_de_novo != 's' and tentar_de_novo != 'não' \
+                        and tentar_de_novo != 'nao' and tentar_de_novo != 'n':
+                    print('')
+                    print('Desculpe, não entendi. Tente novamente.')
+                    print('')
+                    tentar_de_novo = str(input('Quer tentar de novo? [ S/N ] ')).strip().lower()
+                if tentar_de_novo == 's' or tentar_de_novo == 'sim':
+                    vitorias = 0
+                else:
+                    break
+            else:
+                print(f'Você venceu {vitorias} {"vez" if vitorias == 1 else "vezes"}')
+                break
     print('-' * 56)
-    if opcao == ganhador:
-        print('Você VENCEU!')
-        print('Vamos Jogar novamente...')
-        vitorias += 1
-    else:
-        print('Você PERDEU!')
-        break
-print('-=' * 29)
-print(f'GAME OVER! Você venceu {vitorias} {"vez" if vitorias == 1 else "vezes"}')
